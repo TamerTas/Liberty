57799a2b009d3d4de1e59d6127df10fc187974b5
Tamer TAS <TamerTas@users.noreply.github.com>
2015-03-15 21:01:30 +0200

    Include more settings

--- src/test/scala/LibertySpec.scala
+++ src/test/scala/LibertySpec.scala
@@ -4,7 +4,8 @@ object ExampleLibrary extends App {
   val settings = Seq(
     Setting("organization" -> "com.example"),
     Setting("version" -> "0.1.0"),
-    Setting("scalaVersion" -> "2.11.4")
+    Setting("scalaVersion" -> "2.11.4"),
+    Setting("JVMDaemon" -> true)
   )
   val dependencies = Seq(
     Dependency("org.scala-lang.modules", "scala-async", "0.9.2"),
@@ -13,6 +14,5 @@ object ExampleLibrary extends App {
 
   val libertyContext = new LibertyContext(settings, dependencies)
 
-  libertyContext.incrementallyCompile("src/main/scala", "target")
+  libertyContext.incrementallyCompile("src", "target")
 }
-
